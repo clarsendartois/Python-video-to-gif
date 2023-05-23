35569acd68f5ad9420c1b5528729f45f149110ad
Clarsen D'Artois <dartoisclarsen91@gmail.com>
2023-05-22 20:56:40 -0400

    modified

--- video_to_gif_techvidvan.py
+++ video_to_gif_techvidvan.py
@@ -1,27 +1,33 @@
-# import libraries
-from moviepy.editor import*
+#import libraries
+from moviepy.editor import *
 from tkinter import *
-import tkinter.filedialog as tkfd
 import tkinter as tk
+from tkinter import filedialog
 import os
 
-window = Tk() # creating window
-window.geometry("700x300") # geometry of window
-window.title("TechVidvan") # tittle to window
-Label(window, text="Let's make a GIF", font=("bold", 20)).pack() # Label
-
+window = Tk()#creating window
+window.geometry('700x300')#geomtry of window
+window.title('TechVidvan')#title to window
+Label(window,text="Let's make a GIF",font=('bold',20)).pack()#label
 
 def browseFiles():
     global filepath
-    filename = tkfd.askopenfilenames(title="select", filetypes=[("all video fromat", ".mov"), ("all video fromat", ".flv"), ("all video fromat", ".avi"), ("all video fromat", ".mp4")])
-    filepath = os.path.basename(filename)
+    filename = filedialog.askopenfilenames(title='select', filetypes=[
+                    ("all video format", ".mov"),
+                    ("all video format", ".flv"),
+                    ("all video format", ".avi"),
+                    ("all video format", ".mp4"),
+                ])
+    filepath=os.path.basename(filename)
 
 def create_gif():
     clip = VideoFileClip(filepath)
-    clip.write_gif("mygif.gif") # Making a gif
+#     clip.write_gif("mygif.gif")#making a gif
+    # gif=VideoFileClip("mygif.gif")
+    # Label(window,text="Gif Created and Saved").pack()
 
-Label(window, text="Select a File").pack()
-Button(window, text="Browse", command=browseFiles).pack() # button field
-Button(window, text="Create a Dif").pack()# Button field
+Label(window,text='Select a File').pack()
+Button(window,text='Browse',command=browseFiles).pack()#button field
+Button(window,text='Create a Gif',command=create_gif,bg='yellow').pack()#button field
 
-window.mainloop()
+window.mainloop()#display window
