c84f254d68e37405735d34ad9f3c25da2d34469f
Clarsen D'Artois <dartoisclarsen91@gmail.com>
2023-05-25 06:59:08 -0400

    modified

--- video_to_gif.py
+++ video_to_gif.py
@@ -9,9 +9,15 @@ ctk.set_default_color_theme("blue")
 
 font_style_text1 = ("Bookman Old Style", 60, "bold")
 font_style_text2 = ("Bookman Old Style", 30)
+font_style_button1 = ("Bookman Old Style", 25, "bold")
+font_style_button2 = ("Bookman Old Style", 40, "bold")
+
 
 bg_fg_color = "#2b2b2b"
 text_color = "#FFFFFF"
+text_button1_color = "#f97583"
+text_button1_color2 = "#37b78f"
+
 
 text_text1 = "Let's make a GIF"
 text_text2 = "Select a File"
@@ -26,6 +32,7 @@ class VideoToGif:
 
         self.frame = self.create_frame()
         self.text = self.create_text()
+        self.button = self.create_button()
 
     def create_frame(self):
         main_frame = ctk.CTkFrame(
@@ -39,7 +46,16 @@ class VideoToGif:
 
         text2 = tk.Label(self.frame, text=text_text2, font=font_style_text2,
                          background=bg_fg_color, foreground=text_color)
-        text2.place(relx=0.5, rely=0.5, x=-100, y=-150)
+        text2.place(relx=0.5, rely=0.5, x=-120, y=-150)
+
+    def create_button(self):
+        button1 = ctk.CTkButton(self.frame, text="Brwose", font=font_style_button1,
+                                text_color=text_button1_color, border_width=2, width=10)
+        button1.place(relx=0.5, rely=0.5, x=-52, y=-50)
+
+        button2 = ctk.CTkButton(self.frame, text="Create a GIF", font=font_style_button2,
+                                text_color=text_button1_color2, border_width=2, width=10)
+        button2.place(relx=0.5, rely=0.5, x=-135, y=10)
 
     def run(self):
         self.window.mainloop()
